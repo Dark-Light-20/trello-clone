636d4ba3c9491541e001d090b20be040965d07eb
Dark-Light-20 <pazsebas@hotmail.com>
2023-09-14 00:24:20 -0500

    feat: Added dynamic accordions

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -27,4 +27,22 @@ export class BoardsComponent {
   readonly membersIcon = faUsers;
   readonly settingsIcon = faGear;
 
+  items = [
+    {
+      label: 'Item 1',
+      items: [{ label: 'Sub Item 1.1' }, { label: 'Sub Item 1.2' }],
+    },
+    {
+      label: 'Item 2',
+      items: [{ label: 'Sub Item 2.1' }],
+    },
+    {
+      label: 'Item 3',
+      items: [
+        { label: 'Sub Item 3.1' },
+        { label: 'Sub Item 3.2' },
+        { label: 'Sub Item 3.3' },
+      ],
+    },
+  ];
 }
