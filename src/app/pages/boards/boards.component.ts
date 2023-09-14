2df8a88abaa4b655edc33b383b8b0a05af141f3d
Dark-Light-20 <pazsebas@hotmail.com>
2023-09-13 23:26:32 -0500

    feat: Added layout in boards page

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { faHouse, faClock } from '@fortawesome/free-solid-svg-icons';
+import { faElementor, faTrello } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-boards',
-  templateUrl: './boards.component.html'
+  templateUrl: './boards.component.html',
 })
 export class BoardsComponent {
-
+  readonly boardIcon = faTrello;
+  readonly templateIcon = faElementor;
+  readonly homeIcon = faHouse;
+  readonly recentIcon = faClock;
 }
